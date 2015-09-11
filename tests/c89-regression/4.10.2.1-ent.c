a29620f268d3ffc596954dda0dd5a99fa08da52b
Kate F <kate@elide.org>
2015-09-11 14:41:36 +0000

    Add PCG as a workaround.

--- tests/c89-regression/4.10.2.1-ent.c
+++ tests/c89-regression/4.10.2.1-ent.c
@@ -12,6 +12,64 @@
 #define M_PI 3.14159265358979323846
 #endif
 
+#if WORKAROUND
+
+/*
+ * PCG Random Number Generation for C.
+ *
+ * Copyright 2014 Melissa O'Neill <oneill@pcg-random.org>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * For additional information about the PCG random number generation scheme,
+ * including its license and other licensing options, visit
+ *
+ *       http://www.pcg-random.org
+ */
+
+#include <stdint.h>
+
+#undef RAND_MAX
+#define RAND_MAX UINT32_MAX
+
+struct pcg_state_setseq_64 {
+	uint64_t state;
+	uint64_t inc;
+};
+typedef struct pcg_state_setseq_64 pcg32_random_t;
+
+static uint32_t
+pcg32_random_r(pcg32_random_t* rng)
+{
+	uint64_t oldstate = rng->state;
+	rng->state = oldstate * 6364136223846793005ULL + rng->inc;
+	uint32_t xorshifted = ((oldstate >> 18u) ^ oldstate) >> 27u;
+	uint32_t rot = oldstate >> 59u;
+	return (xorshifted >> rot) | (xorshifted << ((-rot) & 31));
+}
+
+static void
+pcg32_srandom_r(pcg32_random_t* rng, uint64_t initstate, uint64_t initseq)
+{
+	rng->state = 0U;
+	rng->inc = (initseq << 1u) | 1u;
+	pcg32_random_r(rng);
+	rng->state += initstate;
+	pcg32_random_r(rng);
+}
+
+#endif
+
 static double
 error(double experimental, double theoretical)
 {
@@ -24,14 +82,26 @@ main(int argc, char *argv[])
 	struct rt_stats r;
 	int i, n = 12345678; /* arbitary sample size */
 
+#if WORKAROUND
+	pcg32_random_t pcg32;
+#endif
+
+#if WORKAROUND
+	pcg32_srandom_r(&pcg32, 0, 1);
+#else
 	srand(0);
+#endif
 
 	rt_init(0);
 
 	for (i = 0; i <= n; i++) {
 		int r;
 
+#if WORKAROUND
+		r = pcg32_random_r(&pcg32);
+#else
 		r = rand();
+#endif
 
 		rt_add(&r, sizeof r);
 	}
